ec2d0af25b964f9bdcb3148a285221ec5a55fa90
Shubham Singh <shubham2101@gmail.com>
2023-08-13 09:32:23 +0530

    two sum Better

--- arraysMedium.js
+++ arraysMedium.js
@@ -2,7 +2,7 @@
 
 // Brute force
 
-function twoSum(arr, target) {
+function twoSumBrute(arr, target) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === target) {
@@ -13,4 +13,39 @@ function twoSum(arr, target) {
   return false;
 }
 
-console.log(twoSum([2, 6, 5, 8, 11], 14));
+// console.log(twoSumBrute([2, 6, 5, 8, 11], 14));
+// TC O(n*n)
+
+function twoSumBetter(arr, target) {
+  let hash = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (hash.has(target - arr[i])) {
+      return true;
+    } else {
+      hash.set(arr[i], i);
+    }
+  }
+  return false;
+}
+// console.log(twoSumBetter([2, 6, 5, 8, 11], 14));
+//TC O(n)
+
+// using object
+
+function twoSum(nums, target) {
+  const numObject = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (complement in numObject) {
+      return [numObject[complement], i]; //return the indices
+    }
+
+    numObject[nums[i]] = i; // Store the number and its index in the object
+  }
+
+  return [];
+}
+
+//TC O(n)
