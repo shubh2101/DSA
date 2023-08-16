b936cdba642d299360ef06139f12f1ef05e9db9a
Shubham Singh <shubham2101@gmail.com>
2023-08-16 20:35:33 +0530

    max subarray sum BETTER

--- arraysMedium.js
+++ arraysMedium.js
@@ -262,4 +262,20 @@ function maxSubarraySumBrute(arr) {
   return maxi;
 }
 
-console.log(maxSubarraySumBrute([-2, -3, 4, -1, -2, 1, 5, -3]));
+// console.log(maxSubarraySumBrute([-2, -3, 4, -1, -2, 1, 5, -3]));
+
+// Better
+
+function maxSubarraySumBetter(arr) {
+  let maxi = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      maxi = Math.max(maxi, sum);
+    }
+  }
+  return maxi;
+}
+
+console.log(maxSubarraySumBetter([-2, -3, 4, -1, -2, 1, 5, -3]));
