6770421b8b5756a4ee8a7f1e1f7fd2c8ef6e8e8a
Shubham Singh <shubham2101@gmail.com>
2024-07-23 18:08:15 +0530

    floyd triangle

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -1,3 +1,4 @@
+//PATTERN 1
 // 1
 // 1 2
 // 1 2 3
@@ -8,6 +9,23 @@ for (let i = 1; i <= 5; i++) {
   let str = '';
   for (let j = 1; j <= i; j++) {
     str = str + j;
+  }
+  //   console.log(str);
+}
+
+// PATTERN 2 :Floyd triangle
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+let k = 1;
+for (let i = 1; i <= 5; i++) {
+  let str = '';
+  for (let j = 1; j <= i; j++) {
+    str += k;
+    k++;
   }
   console.log(str);
 }
